7132b4400b2f04033ee1ef5dd7f8678bbbcf8495
Javier Caballero <caballeroperezjavier@gmail.com>
2023-03-02 15:48:16 -0500

    Agregando funcionalidad del carrito de compras

--- js/productos.js
+++ js/productos.js
@@ -1,11 +1,14 @@
 // Definimos una variable global para llevar la cuenta de los productos creados
 let idProducto = 1;
+let totalCompra = 0;
 
 // Obtenemos los elementos del formulario
 const formulario = document.querySelector('form');
 const inputNombre = formulario.querySelector('#nombre');
 const inputPrecio = formulario.querySelector('#precio');
 const inputDescripcion = formulario.querySelector('#descripcion');
+const listaProd = [];
+const carrito = [];
 
 // Obtenemos el elemento UL donde se mostrarán los productos
 const listaArticulos = document.getElementById('lista-articulos');
@@ -21,9 +24,11 @@ function crearArticulo(nombre, precio, descripcion) {
     id: idProducto++,
     nombre: nombre,
     precio: precio,
-    descripcion: descripcion
+    descripcion: descripcion,
+    cantidad: 1 // Establecemos la cantidad en 1 por defecto
   };
-
+  listaProd.push(producto)
+  console.log(listaProd)
   // Creamos el HTML para mostrar la información del artículo
   const contenidoHTML = `
   <div class="card w-100" style="width: 18rem;">
@@ -31,9 +36,9 @@ function crearArticulo(nombre, precio, descripcion) {
       <h4 class="card-title">${producto.nombre}</h4>
       <p class="card-text">Precio: $${producto.precio}</p>
       <p class="card-text">${producto.descripcion}</p>
-      <input type="number" class="form-control w-25" id="cantidad" name="cantidad"><br>
+      <input type="number" class="form-control w-25" id="c-${producto.id}" name="cantidad"><br>
       <button type="button" class="btn btn-danger btn-sm mb-2" data-mdb-toggle="tooltip" title="Mover a deseados">Me gusta</button>
-      <a href="#" class="btn btn-info btn-sm mb-2" data-mdb-toggle="tooltip" title="Agregar al carrito">Agregar</a>
+      <a href="#" id="p-${producto.id}" class="btn btn-info btn-sm mb-2" data-mdb-toggle="tooltip" title="Agregar al carrito">Agregar</a>
     </div>
   </div>
   `;
@@ -46,6 +51,12 @@ function crearArticulo(nombre, precio, descripcion) {
 
   // Limpiamos el formulario
   formulario.reset();
+
+const btnAgregar = document.getElementById(`p-${producto.id}`);
+btnAgregar.addEventListener("click", () => {
+  agregarAlCarrito(producto.id)
+  console.log(carrito)
+})
 }
 
 // Agregamos un evento al botón "Crear artículo" para crear un nuevo artículo
@@ -55,4 +66,59 @@ document.getElementById('crear-articulo').addEventListener('click', function(eve
   const precio = inputPrecio.value;
   const descripcion = inputDescripcion.value;
   crearArticulo(nombre, precio, descripcion);
+});
+
+//agregamos productos al carrito
+function agregarAlCarrito(id) {
+  const producto = listaProd.find((producto) => producto.id === id);
+  const productoEnCarrito = carrito.find((producto) => producto.id === id);
+  const cantidad = parseInt(document.getElementById(`c-${producto.id}`).value);
+
+  if (productoEnCarrito) {
+    alert("Item duplicado")
+  }
+  else{
+    carrito.push({...producto, cantidad: cantidad});
+    totalCompra += cantidad * producto.precio;
+    actualizarCarrito();
+  }
+}
+// Mostramos el carrito
+const btnRealizarCompra = document.getElementById('realizar-compra');
+const divCarrito = document.getElementById('carrito');
+const listaCarrito = document.getElementById('lista-carrito');
+
+btnRealizarCompra.addEventListener('click', () => {
+  // Mostrar el contenido del carrito
+  divCarrito.style.display = 'block';
+
+  // Limpiar la lista de elementos de carrito
+  listaCarrito.innerHTML = '';
+
+  // Agregar los elementos del carrito a la lista
+  carrito.forEach((producto) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.innerHTML = `
+      <div class="d-flex justify-content-between align-items-center">
+        <span>${producto.nombre}</span>
+        <span>$${producto.precio}</span>
+      </div>
+    `;
+    listaCarrito.appendChild(li);
+  });
+});
+
+//Vaciar el carrito
+const btnVaciarCarrito = document.getElementById('vaciar-carrito');
+
+btnVaciarCarrito.addEventListener('click', () => {
+  // Limpiar el carrito
+  carrito.length = 0;
+
+  // Ocultar el contenido del carrito
+  divCarrito.style.display = 'none';
+
+  // Limpiar la lista de elementos de carrito
+  listaCarrito.innerHTML = '';
 });
